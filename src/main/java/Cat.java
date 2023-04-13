0d9f08c04eb32dd4adafcf148082dda96a578eda
Mardali <mardali.tamalov@mail.ru>
2023-04-13 14:54:06 +0300

    task1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,8 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cat {
+    private String message2;
+}
---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,19 @@
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class HelloWorld {
+ 
+    private String message;
+ 
+  /*  public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+     */
+}
